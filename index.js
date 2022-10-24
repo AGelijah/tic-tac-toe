014f612c90c244edd2b974223abfbcc76a65a86b
Glorious <gloriouselijah125@gmail.com>
2022-10-24 09:53:59 +0000

    :bug: Player turn
    if restarted the token turn box freezes on token 'o' for about 2 rounds

--- index.js
+++ index.js
@@ -12,13 +12,21 @@ const $cpuStart = $("#cpu-start");
 const $humanStart = $("#human-start");
 const $playerXName = $("#x-score");
 const $playerOName = $("#o-score");
+const $playerTurn = $("#player-turn");
+const $tile = $(".tile");
+const $tileToken = $(".tile-token");
+const $tokenX = $(".token-x");
+const $tokenO = $(".token-o");
 
 let playerOneToken = "x";
 let playerOneName = "p1";
 let playerTwoToken = "o";
 let playerTwoName = "p2";
+let xToken = "/assets/icon-x.svg";
+let oToken = "/assets/icon-o.svg";
 let cpuName = "cpu";
 let gameMode;
+let roundsPlayed = 0;
 
 function gameModeCheck(gameMode) {
   if (gameMode === "human") {
@@ -39,30 +47,20 @@ function gameModeCheck(gameMode) {
     }
   }
 }
+function playerTurn(round) {
+  if (round % 2 === 0) {
+    $("#player-token-o").show();
+    $("#player-token-x").hide();
+    return;
+  }
+  if (round % 2 === 1) {
+    $("#player-token-x").show();
+    $("#player-token-o").hide();
+    return;
+  }
+}
 
 function main() {
-  // //variables
-  // const $restartGame = $("#restart-game");
-  // const $endGame = $("#end-game");
-  // const $restartButton = $("#restart-button");
-  // const $noRestart = $("#no-restart");
-  // const $yesRestart = $("#yes-restart");
-  // const $gameScreen = $("#cover");
-  // const $welcomeScreen = $("#welcome-screen");
-  // const $playerX = $("#player-x");
-  // const $playerO = $("#player-o");
-  // const $cpuStart = $("#cpu-start");
-  // const $humanStart = $("#human-start");
-  // const $playerXName = $("#x-score");
-  // const $playerOName = $("#o-score");
-
-  // let playerOneToken = "x";
-  // let playerOneName = "p1";
-  // let playerTwoToken = "o";
-  // let playerTwoName = "p2";
-  // let cpuName = "cpu";
-  // let gameMode;
-
   $restartGame.hide();
   $endGame.hide();
   $gameScreen.hide();
@@ -91,6 +89,43 @@ function main() {
     $welcomeScreen.hide();
     $gameScreen.show();
   });
+  $tile.on("click", (e) => {
+    if ($($(e.currentTarget).children()[0]).is(":visible")) {
+      alert("Invalid Move");
+      return;
+    }
+    playerTurn(roundsPlayed);
+    if (roundsPlayed % 2 === 1) {
+      $(e.currentTarget).prepend(
+        `<img src="/assets/icon-o.svg" alt="" class="token" />`
+      );
+      $(e.currentTarget).removeClass("x-outline");
+      $(e.currentTarget).removeClass("o-outline");
+    } else {
+      $(e.currentTarget).prepend(
+        `<img src="/assets/icon-x.svg" alt="" class="token" />`
+      );
+      $(e.currentTarget).removeClass("x-outline");
+      $(e.currentTarget).removeClass("o-outline");
+    }
+    roundsPlayed += 1;
+  });
+  $tile.on("mouseenter", (e) => {
+    if (!$($(e.currentTarget).children()[0]).is(":visible")) {
+      if (roundsPlayed % 2 === 1) {
+        $(e.currentTarget).addClass(" o-outline");
+      } else {
+        $(e.currentTarget).addClass(" x-outline");
+      }
+    }
+  });
+  $tile.on("mouseleave", (e) => {
+    if (roundsPlayed % 2 === 1) {
+      $(e.currentTarget).removeClass(" o-outline");
+    } else {
+      $(e.currentTarget).removeClass(" x-outline");
+    }
+  });
 
   $restartButton.on("click", () => {
     $restartGame.show();
@@ -99,6 +134,9 @@ function main() {
     $restartGame.hide();
   });
   $yesRestart.on("click", (e) => {
+    roundsPlayed = 0;
+    playerTurn(roundsPlayed);
+    $tile.children().remove();
     $restartGame.hide();
   });
 }
